f204cbf713fffb66245dd851c0c00bef6c10190c
Brian Meneley <brianmeneley@gmail.com>
2026-01-16 22:47:53 -0400

    chore: start terraform modules for talos cluster

---
+++ terraform/README.md
@@ -0,0 +1,5 @@
+# `terraform`
+
+Main terraform entrypoint for deploying talos cluster on proxmox.
+
+Reusable modules go in the `modules/` folder.
---
+++ terraform/environments/hera/talos-cluster.tf
@@ -0,0 +1,5 @@
+# ideally just call outs to modules in `modules/` setting env-specific config
+
+# TODO: talos control plane VM
+
+# TODO: talos worker node
